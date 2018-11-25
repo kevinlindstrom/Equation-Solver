f2d015c1873eab032cdf55a325be728c6cbf56f2
Admin <kevin.lindstrom.ct@gmail.com>
2018-11-25 14:07:11 -0500

    can use with img num

--- systems_of_eq.m
+++ systems_of_eq.m
@@ -1,20 +1,20 @@
 syms v1 v2 v3 v4 v5 v6 v7 test i1 i2 i3 i4 i5 i6 i7
 %initial parameters to be substituted
-ix = (v1+4)/8;
-vx = (v1+2)/16;
-id = (30-v1)/3;
+ib = sqrt(2);
+ia = 2;
+
 %equations to be solved
-equation1 = 0 == (v1-v2)/7-60+(v1-10)/3;
-equation2 = 0 == (v2-v1)/7+30+(v2-4*id)/2+(v2-30)/3;
-equation3 = 0 == v3-v2-15;
-equation4 = 0 == 4*i3+10*i4+2-9;
+equation1 = 0 == (-3j+1j+5)*i1+3j*ib-1j*i2;
+equation2 = 0 == (1j-2j+5)*i2-1j*i1;
+equation3 = 0 == v2;
+equation4 = 0 == i4;
 equation5 = 0 == v5;
 
 %solve parameters
 number_of_equations = 2;
 number_of_variables = 2;
 test_exist = false;
-use_voltages = true;
+use_voltages = false;
 
 %items to be iterated over
 list_eqs = [equation1, equation2, equation3, equation4, equation5];
